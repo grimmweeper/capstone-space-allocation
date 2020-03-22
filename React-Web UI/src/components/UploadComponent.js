80e4bbe8099a3c5056f9ab7a62a50043832c14a8
weepz <drantinire@gmail.com>
2020-03-22 18:38:23 +0800

    added upload CSV and GET functions to Web UI

--- React-Web UI/src/components/UploadComponent.js
+++ React-Web UI/src/components/UploadComponent.js
@@ -1,59 +1,75 @@
-import React from "react";
+import React, { Component } from 'react';
 import CSVReader from "react-csv-reader";
 import { Breadcrumb, BreadcrumbItem, Row, Col, Button } from 'reactstrap';
 
-var uploadCSV = (data) => {
-
-    var data_json = JSON.stringify(data);
-
-    console.log(data_json);
-    return data_json;
-}
-
 const papaparseOptions = {
-  header: true,
-  dynamicTyping: true,
-  skipEmptyLines: true,
-  transformHeader: header => header.toLowerCase()
-};
+    header: true,
+    dynamicTyping: true,
+    skipEmptyLines: true,
+    transformHeader: header => header.toLowerCase()
+  };
 
-function postCSV() {
-    fetch('http://localhost:3001/crud', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            first: 'lala',
-            last: 'land',
-            email: 'hello me',
-            phone: 75844,
-            location:"here",
-            hobby:"motherfucker"
+
+class Upload extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {data:[]};
+    }
+
+    uploadCSV = data => {
+
+        //var data_json = JSON.stringify(data);
+        var data_json = data;
+    
+        console.log(data_json);
+        this.setState({data: data_json});
+    }
+
+    postCSV = () => {
+
+        console.log(this.state.data[0]);
+        fetch('http://localhost:3001/crud', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            // body: JSON.stringify({
+            //     first: 'lala',
+            //     last: 'land',
+            //     email: 'hello me',
+            //     phone: 75844,
+            //     location:"here",
+            //     hobby:"nothing :)"
+            // })
+            body: JSON.stringify(this.state.data[0])
         })
-    })
+    }
+
+    render() {
+        return(
+            <div className="container">
+                <CSVReader
+                    cssClass="react-csv-input"
+                    label="Upload CSV with requirement details"
+                    onFileLoaded={this.uploadCSV}
+                    parserOptions={papaparseOptions}
+                />
+                <Row className="form-group">
+                    <Col md={{size:10}}>
+                        <Button type="submit" color="primary" onClick = { this.postCSV }>
+                        Upload
+                        </Button>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
+    
+
+    
 }
 
-
-function Upload(props) {
-    return(
-        <div className="container">
-            <CSVReader
-                cssClass="react-csv-input"
-                label="Upload CSV with requirement details"
-                onFileLoaded={uploadCSV}
-                parserOptions={papaparseOptions}
-            />
-            <Row className="form-group">
-                <Col md={{size:10}}>
-                    <Button type="submit" color="primary" onClick = { postCSV }>
-                    Upload
-                    </Button>
-                </Col>
-            </Row>
-        </div>
-    );
-}
 
 export default Upload;
